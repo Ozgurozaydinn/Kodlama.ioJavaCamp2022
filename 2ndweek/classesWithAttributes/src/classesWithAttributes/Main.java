4223ac776c07bc35a01ca5c0b069fa02a1be1016
Ozgurozaydinn <ozgurozaydin10@gmail.com>
2022-09-26 23:45:43 +0300

    Applied from the playlist to the 33rd video.

---
+++ 2ndweek/classesWithAttributes/src/classesWithAttributes/Main.java
@@ -0,0 +1,25 @@
+package classesWithAttributes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Product product = new Product();
+		product.setName("Laptop");
+		product.setId(1);
+		product.setDescription("Asus Laptop");
+		product.setPrice(5000);
+		product.setStockAmount(3);
+		product.setRenk("");
+
+		System.out.println(product.getKod());
+
+		ProductManager productManager = new ProductManager();
+		productManager.Add(product);
+		productManager.Add2(5, "", "", 2, 200);
+		productManager.Add2(5, "", "", 2, 200);
+		productManager.Add2(5, "", "", 2, 200);
+		productManager.Add2(5, "", "", 2, 200);
+		productManager.Add2(5, "", "", 2, 200);
+	}
+
+}
